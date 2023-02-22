55cd876b19e9a8d27c13e6e6de6fc05642dcd8c7
Benjamin Lepourtois <benjamin.lepourtois@gmail.com>
2023-02-22 14:25:23 +0100

    Sorting and adding the MCP23008 test

---
+++ Applications/test_MCP23008/main.cpp
@@ -0,0 +1,32 @@
+#include "stm32f3xx.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ST7735.h"
+#include "tft.h"
+
+/** affichage d'une valeur 
+ *  au format virgule fixe 10.6
+ */
+void affVF10_6(int16_t val)
+{
+	//seul endroit avec des 'float' car c'est du debug!!
+	Tft.print(((float)(val))/64);
+}
+
+void setup() {
+    Tft.setup();
+}
+
+int main(void) {
+
+    volatile int temps;
+    setup();
+
+    Tft.setTextCursor(5,0);
+    Tft.print("Coucou Leila !");
+
+    /* Infinite loop */
+    while (1) {
+
+    }
+}
+
